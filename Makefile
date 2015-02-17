4f6e04225b001cea2cf5734b1672817aa4583152
Andrea Corna <andrea.corna.ac.91@gmail.com>
2015-02-17 16:26:40 +0100

    add openmp
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: main
 
 main: main.o
-	g++ main.o  `pkg-config --libs opencv` -o main
+	g++ main.o  -fopenmp `pkg-config --libs opencv` -o main
 	
 main.o: main.cpp
 	g++ -c main.cpp -fopenmp  `pkg-config opencv --cflags`  
--- main.cpp
+++ main.cpp
@@ -25,34 +25,31 @@ int main( int argc, char** argv ){
 	Mat image;
 	int i;
 	int j;
-	double inverse_gamma = 1.0 / 2.0;
+	double inverse_gamma = 1.0 / 2.2;
 	image = imread(IMAGE_PATH,1);
-	
-	for(i=0;i<image.rows;i++){
-		
-		int* row = image.ptr<int>(i);
-		for(j=0;j<image.cols;j++){
-			if (j==0 && i==0){
-				cout << "Before: ";
-				cout << row[j] << endl;
-			}
-		//	row[j] =  255 *(pow((double)row[j]/255,inverse_gamma ));
-			//row[j] = (int)( pow( (double) row[j] / 255.0, inverse_gamma ) * 255.0 );;
-		
-			if (j==0 && i==0){
-				cout << "After: ";
-				cout << row[j] << endl;
-			}
+	#pragma omp parallel for shared(image)
+		for(i=0;i<image.rows;i++){
+			
+			int* row = image.ptr<int>(i);
+			#pragma omp parallel for shared(row)
+				for(j=0;j<image.cols;j++){
+					
+				
+					row[j] = (int)( pow( (double) row[j] / 255, inverse_gamma ) * 255.0 );
+				
+				
+				}
+			
+			
+			
 		}
-		
-		
-	}
 	
-	Mat image2 = correctGamma(image,2.0);
+		
+	//Mat image2 = correctGamma(image,2.0);
 
 	
 	//Mat gray_image;
 	//cvtColor( image, gray_image, CV_BGR2GRAY );
 
-	imwrite( "./mod_image.jpg", image2 );
+	imwrite( "./mod_image.jpg", image );
 }
