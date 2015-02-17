e8298b6e68d173d978b30e36234dcee15bd45385
Fons91 <lord.fontana@gmail.com>
2015-02-17 15:23:33 +0100

    stub opencv
    Signed-off-by: Fons91 <lord.fontana@gmail.com>

---
+++ Makefile
@@ -0,0 +1,10 @@
+all: main
+
+main: main.o
+	g++ main.o  `pkg-config --libs opencv` -o main
+	
+main.o: main.cpp
+	g++ -c main.cpp -fopenmp  `pkg-config --cflags`  
+	
+clean:
+	rm *o main
--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include "omp.h"
+#include <cv.h>
+#include <highgui.h>
+#define IMAGE_PATH "./test.jpg"
 
-int main(){
-		cout << "Hello";
+using namespace cv;
+
+int main( int argc, char** argv ){
+	Mat image;
+	image = imread(IMAGE_PATH,1);
+	
+	Mat gray_image;
+	cvtColor( image, gray_image, CV_BGR2GRAY );
+
+	imwrite( "./Gray_Image.jpg", gray_image );
 }
