4d55c0e2344bb2b62bc6fadc49142fdba28e3a13
Fons91 <lord.fontana@gmail.com>
2015-02-17 15:38:29 +0100

    test value image
    Signed-off-by: Fons91 <lord.fontana@gmail.com>

--- main.cpp
+++ main.cpp
@@ -8,10 +8,21 @@ using namespace cv;
 
 int main( int argc, char** argv ){
 	Mat image;
+	int i;
+	int j;
 	image = imread(IMAGE_PATH,1);
 	
-	Mat gray_image;
-	cvtColor( image, gray_image, CV_BGR2GRAY );
+	for(i=0;i<image.rows;i++){
+		
+		int* row = image.ptr<int>(i);
+		for(j=0;j<image.cols;j++){
+			cout << row[j];
+		}
+	}
+	
+	
+	//Mat gray_image;
+	//cvtColor( image, gray_image, CV_BGR2GRAY );
 
-	imwrite( "./Gray_Image.jpg", gray_image );
+	//imwrite( "./Gray_Image.jpg", gray_image );
 }
