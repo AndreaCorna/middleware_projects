098b92f40db9dfa8a69580a11f0367177cf69260
Andrea Corna <andrea.corna.ac.91@gmail.com>
2015-02-20 00:03:29 +0100

    add comment openmp
    Signed-off-by: Andrea Corna <andrea.corna.ac.91@gmail.com>

--- openmp_mpi/main.cpp
+++ openmp_mpi/main.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 using namespace std;
 
 
-
+/*Code for gamma correction for a single pixel*/
 int gammaCorrectPixel(int pixel,double inverse_gamma){	
 	return (int)( pow( (double) pixel / 255, inverse_gamma ) * 255.0 );
 }
@@ -32,52 +32,45 @@ int main( int argc, char** argv ){
 	if(rank == 0){
 		image = imread(IMAGE_PATH,1);
 		buffer_image=image.ptr();
-		 nrows = image.rows;
-		 ncol = image.cols;
-		 channels = image.channels();
-		 buffer_size = ncol*nrows*channels;
-		 item_for_process = (int) buffer_size/number_processes;
+		nrows = image.rows;
+		ncol = image.cols;
+		channels = image.channels();
+		buffer_size = ncol*nrows*channels;
+		item_for_process = (int) buffer_size/number_processes;
 		cout<<"i've receive "<<item_for_process<<endl;
 	}
-
+	//Broadcast of number of pixel to be processed by every single machine
 	MPI_Bcast(&item_for_process,1,MPI_INT,0,MPI_COMM_WORLD);
 
     
 	cout<<"out "<<item_for_process<<endl;
 
-	    
+	//buffer for elaboration
 	uchar *gamma_corrected_buffer = (uchar *)malloc((sizeof(uchar))*item_for_process);
-	   
-   MPI_Scatter(buffer_image, item_for_process, MPI_UNSIGNED_CHAR, gamma_corrected_buffer,item_for_process, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+	//Scatter of the data 
+    MPI_Scatter(buffer_image, item_for_process, MPI_UNSIGNED_CHAR, gamma_corrected_buffer,item_for_process, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
     
+	/*gamma correction with omp elaboration*/
+	#pragma omp parallel for shared(gamma_corrected_buffer)
+		for(i=0;i < item_for_process;i++){
+			gamma_corrected_buffer[i] = gammaCorrectPixel(gamma_corrected_buffer[i],inverse_gamma);		
+			
+		}
 	
-	for(i=0;i < item_for_process;i++){
-		gamma_corrected_buffer[i] = gammaCorrectPixel(gamma_corrected_buffer[i],inverse_gamma);		
-		
-	}
 	uchar* dest = 0;
 	if(rank == 0){
 			dest = (uchar*)malloc((sizeof(uchar))*buffer_size);
 	}
-	
+	//collect all the data
 	MPI_Gather(gamma_corrected_buffer, item_for_process, MPI_UNSIGNED_CHAR, dest, item_for_process, MPI_UNSIGNED_CHAR, 0,MPI_COMM_WORLD);
  
 
 	if(rank == 0){
 		image.data = dest;
 		imwrite( "./mod_image.jpg", image );
-	
 	}
 
 	
 	MPI_Finalize();
 	
-		
-	//Mat image2 = correctGamma(image,2.0);
-
-	
-	//Mat gray_image;
-	//cvtColor( image, gray_image, CV_BGR2GRAY );
-
-	//imwrite( "./mod_image.jpg", image );
 }
